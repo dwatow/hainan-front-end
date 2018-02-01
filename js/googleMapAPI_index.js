a6b75350a8c15b479ef4f45925b8cfb2f0f7614d
wangyuhsiang <kevin10410@gmail.com>
2018-02-01 20:58:53 +0800

    rename feeback js and drae index page from BE data

--- js/googleMapAPI_index.js
+++ js/googleMapAPI_index.js
@@ -1,86 +1,5 @@
 //fake data
-let activityData = [
-    {
-        "cityname": "臺南市", "sealinename": "黃金海岸", "coordinates": [
-            [
-                120.17607431485473,
-                22.930725534782255
-            ],
-            [
-                120.1761817932129,
-                22.92930645370021
-            ]
-        ], "activity": "黃金海岸淨灘", "date": "2018/03/01"
-    },
-    {
-        "cityname": "嘉義縣", "sealinename": "東石橋畔出海口", "coordinates": [
-            [
-                120.17892956729158,
-                23.4647674172182
-            ],
-            [
-                120.17695546145661,
-                23.46342895064905
-            ],
-            [
-                120.17670470547195,
-                23.46118648719332
-            ]
-        ], "activity": "東石橋畔出海口淨灘", "date": "2018/03/02"
-    },
-    {
-        "cityname": "臺東縣", "sealinename": "豐里二號橋", "coordinates": [
-            [
-                121.10264062872375,
-                22.706256209590048
-            ],
-            [
-                121.10674144762326,
-                22.708688485720593
-            ]
-        ], "activity": "豐里二號橋淨灘", "date": "2018/03/03"
-    },
-    {
-        "cityname": "花蓮縣", "sealinename": "奇萊鼻至東防波", "coordinates": [
-            [
-                121.64324820041658,
-                24.018650111280472
-            ],
-            [
-                121.64339840412141,
-                24.01830221583367
-            ],
-            [
-                121.64389729499818,
-                24.017165424148576
-            ],
-            [
-                121.6445678472519,
-                24.015847321207293
-            ],
-            [
-                121.64506673812868,
-                24.014578205608412
-            ],
-            [
-                121.6451183674332,
-                24.014478783106245
-            ]
-        ], "activity": "奇萊鼻至東防波淨灘", "date": "2018/03/04"
-    },
-    {
-        "cityname": "新北市", "sealinename": "龍洞地質公園停車場至龍安廟前海岸", "coordinates": [
-            [
-                121.91305137141984,
-                25.1141317045611
-            ],
-            [
-                121.91292285919191,
-                25.114015418454322
-            ]
-        ], "activity": "龍洞地質公園淨灘", "date": "2018/03/05"
-    }
-];
+let activityData = [];
 
 let reportData = [
     {
@@ -152,6 +71,34 @@ let reportData = [
         ], "clean": "否", "date": "2017/05/04"
     }
 ];
+//get created Report
+$.ajax({
+    url: 'https://hainan-api.oss.tw/api/beach/activity',
+    type: 'GET',
+    dataType: 'json',
+    success: function (response) {
+        activityData = response.result;
+        // console.log(activityData)
+        initIndexMap();
+    },
+    error: function (jqXHR, status, errorThrown) {
+        console.log(jqXHR);
+    }
+})
+
+//get created Feedback
+$.ajax({
+    url: 'https://hainan-api.oss.tw/api/beach/notification',
+    type: 'GET',
+    dataType: 'json',
+    success: function (response) {
+        reportData = response.result;
+        console.log(reportData);
+    },
+    error: function (jqXHR, status, errorThrown) {
+        console.log(jqXHR);
+    }
+})
 
 const displayFilter = document.querySelector('#displayFilter');
 displayFilter.addEventListener('change', display);
@@ -182,7 +129,7 @@ let map;
 let activityInfoWindow;
 let reportInfoWindow;
 function initIndexMap() {
-    console.log('initMap Success')
+    // console.log('initMap Success')
     //HomePage Map
     map = new google.maps.Map(document.getElementById('map'), {
         zoom: 7,
@@ -204,26 +151,29 @@ function dropActivityMarker() {
     removeAllSealine();
     clearResults();
     clearMarkers();
+    // console.log("dropActivityMarker")
     activityData.forEach(function(beach, index){
         let markerLetter = String.fromCharCode('A'.charCodeAt(0) + (index % 26));
         let markerIcon = `./css/GoogleMarkers/green_Marker${markerLetter}.png`;
-        let coord = beach.coordinates.reduce(function (accumulator, currentValue) {
+        let coord = beach.geojson.reduce(function (accumulator, currentValue) {
             // console.log(accumulator, currentValue)
-            return [(accumulator[0]) + (currentValue[0]) / beach.coordinates.length, (accumulator[1]) + (currentValue[1]) / beach.coordinates.length];
+            return [(accumulator[0]) + (currentValue[0]) / beach.geojson.length, (accumulator[1]) + (currentValue[1]) / beach.geojson.length];
         }, [0, 0]);
+
+        let cleanDateArray = beach.dateTime.split('T');
 
         markers[index] = new google.maps.Marker({
             position: { lat: coord[1], lng: coord[0] },
             animation: google.maps.Animation.DROP,
             icon: markerIcon,
-            activity: beach.activity,
-            cityname: beach.cityname,
-            sealinename: beach.sealinename,
-            date: beach.date
+            activity: beach.title,
+            city: beach.city,
+            sealinename: beach.beachTitle,
+            date: cleanDateArray[0]
         });
 
         let googleArray = [];
-        beach.coordinates.forEach(function (coord) {
+        beach.geojson.forEach(function (coord) {
             let coordObj = { lat: coord[1], lng: coord[0] };
             googleArray.push(coordObj);
         });
@@ -248,23 +198,25 @@ function dropReportMarker() {
     reportData.forEach(function(beach, index){
         let markerLetter = String.fromCharCode('A'.charCodeAt(0) + (index % 26));
         let markerIcon = `./css/GoogleMarkers/red_Marker${markerLetter}.png`;
-        let coord = beach.coordinates.reduce(function (accumulator, currentValue) {
+        let coord = beach.geojson.reduce(function (accumulator, currentValue) {
             // console.log(accumulator, currentValue)
-            return [(accumulator[0]) + (currentValue[0]) / beach.coordinates.length, (accumulator[1]) + (currentValue[1]) / beach.coordinates.length];
+            return [(accumulator[0]) + (currentValue[0]) / beach.geojson.length, (accumulator[1]) + (currentValue[1]) / beach.geojson.length];
         }, [0, 0]);
+
+        let feedbackDateArray = beach.updateDate.split('T');
 
         markers[index] = new google.maps.Marker({
             position: { lat: coord[1], lng: coord[0] },
             animation: google.maps.Animation.DROP,
             icon: markerIcon,
-            cityname: beach.cityname,
-            sealinename: beach.sealinename,
-            date: beach.date,
-            clean: beach.clean
+            cityname: beach.city,
+            sealinename: beach.beachName,
+            date: feedbackDateArray[0],
+            clean: beach.beachClean
         });
 
         let googleArray = [];
-        beach.coordinates.forEach(function (coord) {
+        beach.geojson.forEach(function (coord) {
             let coordObj = { lat: coord[1], lng: coord[0] };
             googleArray.push(coordObj);
         });
@@ -315,6 +267,7 @@ const results = document.getElementById('results');
 let reportImage = document.querySelector('.reportImage');
 
 function addResult(result, i, markerIcon) {
+    // console.log(result)
     // let markerLetter = String.fromCharCode('A'.charCodeAt(0) + (i % 26));
     // let markerIcon = iconImage + markerLetter + '.png';
     let tr = document.createElement('tr');
@@ -329,10 +282,13 @@ function addResult(result, i, markerIcon) {
     icon.setAttribute('class', 'placeIcon');
     icon.setAttribute('className', 'placeIcon');
     iconTd.appendChild(icon);
-    if (result.clean === undefined) {
-        nameTd.innerHTML = `活動日期${result.date}-${result.activity}`;
+    if (result.dateTime === undefined) {
+        let feedbackDateArray = result.updateDate.split('T');
+        nameTd.innerHTML = `回報日期${feedbackDateArray[0]}-${result.title}`;
+        
     } else {
-        nameTd.innerHTML = `回報日期${result.date}-${result.sealinename}`;
+        let cleanDateArray = result.dateTime.split('T');
+        nameTd.innerHTML = `活動日期${cleanDateArray[0]}-${result.title}`;
     }
     tr.appendChild(iconTd);
     tr.appendChild(nameTd);
@@ -344,9 +300,10 @@ function showActivityWindow(event) {
     map.setZoom(12);
     map.setCenter(marker.getPosition());
     //開啟infoWindow
+    console.log('marker', marker);
     activityInfoWindow.open(map, marker);
     document.getElementById('activity').innerHTML = `${marker.activity}`;
-    document.getElementById('iw-beach').textContent = marker.sealinename;
+    document.getElementById('iw-beach').textContent = marker. sealinename;
     // document.getElementById('iw-city').textContent = marker.cityname;
     document.getElementById('iw-date').textContent = marker.date;
     // document.getElementById('iw-location').textContent = 'location';
@@ -370,7 +327,12 @@ function showReportWindow(event) {
     reportInfoWindow.open(map, marker);
     document.getElementById('report').innerHTML = `${marker.sealinename}`;
     document.getElementById('iw-reportDate').textContent = marker.date;
-    document.getElementById('iw-clean').textContent = marker.clean;
+    if (marker.clean) {
+        document.getElementById('iw-clean').textContent = "否";
+    } else {
+        document.getElementById('iw-clean').textContent = "是";
+    }
+    
     reportImage.style.backgroundImage = `url(https://i.imgur.com/FtsBIRx.jpg)`;
     showActive({
         beach: "某個海灘",
